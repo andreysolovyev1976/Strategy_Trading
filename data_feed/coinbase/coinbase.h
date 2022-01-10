56ae6e3713017e34b4ba8ec412c8eaf7b7da077f
Andrey Solovyev <andreysolovyev1976@gmail.com>
2022-01-11 01:55:14 +0300

    Intermediate submit, just cleaning up

--- data_feed/coinbase/coinbase.h
+++ data_feed/coinbase/coinbase.h
@@ -16,7 +16,34 @@ namespace algo {
 	namespace coinbase {
 
 
+	  ///Declarations
 	  std::ostream& operator<<(std::ostream& os, const CurrencyData& data_coinbase);
+
+	  template<typename ...Fields>
+	  void Print(const CurrencyData& data_coinbase, std::ostream& os, Fields ...fields);
+	  template<typename ...Fields>
+	  void Print(const std::vector<CurrencyData>& data_coinbase, std::ostream& os, Fields ...fields);
+
+	  Json::Document getListOfCurrencies(bool debug_output = false, std::ostream& os = std::cerr);
+
+	  template <typename Input>
+	  std::vector<CurrencyData> processListOfCurrencies(Input input);
+
+	  Json::Document getSingleCurrency(const types::String& ticker, bool debug_output = false,
+			  std::ostream& os = std::cerr);
+
+	  template <typename Input>
+	  CurrencyData processSingleCurrency(Input input);
+
+	  Json::Document getSingleTradeData(const types::String& ticker1, const types::String& ticker2,
+			  bool debug_output = false, std::ostream& os = std::cerr);
+
+	  template <typename Input>
+	  TradeData processSingleTradeData(Input input);
+
+
+
+	  ///Definitions
 
 	  template<typename ...Fields>
 	  void Print(const CurrencyData& data_coinbase, std::ostream& os, Fields ...fields)
@@ -32,19 +59,142 @@ namespace algo {
 		  }
 	  }
 
-	  Json::Document getListOfCurrencies(bool debug_output = false, std::ostream& os = std::cerr);
-	  std::vector<CurrencyData> processListOfCurrencies(Json::Document document);
-	  std::vector<CurrencyData> processListOfCurrencies(Json::Node node);
 
-	  Json::Document getSingleCurrency(const types::String& ticker, bool debug_output = false,
-			  std::ostream& os = std::cerr);
-	  CurrencyData processSingleCurrency(Json::Document document);
-	  CurrencyData processSingleCurrency(Json::Node node);
+	  template <typename Input>
+	  std::vector<CurrencyData> processListOfCurrencies(Input input)
+	  {
+		  std::vector<CurrencyData> data_coinbase;
 
-	  Json::Document getSingleTradeData(const types::String& ticker1, const types::String& ticker2,
-			  bool debug_output = false, std::ostream& os = std::cerr);
-	  TradeData processSingleTradeData(Json::Document document);
-	  TradeData processSingleTradeData(Json::Node node);
+		  const Json::Node *node;
+		  if constexpr (std::is_same_v<std::decay_t<Input>, Json::Document>) node = &input.GetRoot();
+		  else if constexpr (std::is_same_v<std::decay_t<Input>, Json::Node>) node = &input;
+		  else return data_coinbase;
+
+		  if (not node->IsArray()) return data_coinbase;
+		  auto& currencies = const_cast<Json::Array&>(node->AsArray());
+		  data_coinbase.reserve(currencies.size());
+
+		  for (auto& currency : currencies) {
+			  data_coinbase.emplace_back(processSingleCurrency(std::move(currency)));
+		  }
+
+		  return data_coinbase;
+	  }
+
+
+	  template <typename Input>
+	  CurrencyData processSingleCurrency(Input input)
+	  {
+		  CurrencyData data_coinbase;
+
+		  const Json::Node *node;
+		  if constexpr (std::is_same_v<std::decay_t<Input>, Json::Document>) node = &input.GetRoot();
+		  else if constexpr (std::is_same_v<std::decay_t<Input>, Json::Node>) node = &input;
+		  else return data_coinbase;
+
+		  if (not node->IsDict()) return data_coinbase;
+		  auto& currency = const_cast<Json::Dict&>(node->AsDict());
+		  data_coinbase.empty = false;
+		  if (auto details = currency.find("details"); details!=currency.end()) {
+			  if (details->second.IsDict()) {
+				  auto& details_dict = const_cast<Json::Dict&>(details->second.AsDict());
+
+				  if (auto found = details_dict.find("crypto_address_link");
+						  found!=details_dict.end() && found->second.IsString()) {
+					  data_coinbase.crypto_address_link = std::move(const_cast<std::string&>(found->second.AsString()));
+				  }
+				  if (auto found = details_dict.find("crypto_transaction_link");
+						  found!=details_dict.end() && found->second.IsString()) {
+					  data_coinbase.crypto_transaction_link = std::move(
+							  const_cast<std::string&>(found->second.AsString()));
+				  }
+				  if (auto found = details_dict.find("display_name");
+						  found!=details_dict.end() && found->second.IsString()) {
+					  data_coinbase.display_name = std::move(const_cast<std::string&>(found->second.AsString()));
+				  }
+				  if (auto found = details_dict.find("max_withdrawal_amount");
+						  found!=details_dict.end() && found->second.IsInt()) {
+					  data_coinbase.max_withdrawal_amount = found->second.AsInt();
+				  }
+				  if (auto found = details_dict.find("min_withdrawal_amount");
+						  found!=details_dict.end() && found->second.IsInt()) {
+					  data_coinbase.min_withdrawal_amount = found->second.AsInt();
+				  }
+				  if (auto found = details_dict.find("type");
+						  found!=details_dict.end() && found->second.IsString()) {
+					  data_coinbase.type = std::move(const_cast<std::string&>(found->second.AsString()));
+				  }
+			  }
+		  }
+		  if (auto found = currency.find("id");
+				  found!=currency.end() && found->second.IsString()) {
+			  data_coinbase.id = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = currency.find("max_precision");
+				  found!=currency.end() && found->second.IsString()) {
+			  data_coinbase.max_precision = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = currency.find("min_size");
+				  found!=currency.end() && found->second.IsString()) {
+			  data_coinbase.min_size = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = currency.find("name");
+				  found!=currency.end() && found->second.IsString()) {
+			  data_coinbase.name = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = currency.find("status");
+				  found!=currency.end() && found->second.IsString()) {
+			  data_coinbase.status = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+
+		  return data_coinbase;
+	  }
+
+
+	  template <typename Input>
+	  TradeData processSingleTradeData(Input input)
+	  {
+		  TradeData data_coinbase;
+
+		  const Json::Node *node;
+		  if constexpr (std::is_same_v<std::decay_t<Input>, Json::Document>) node = &input.GetRoot();
+		  else if constexpr (std::is_same_v<std::decay_t<Input>, Json::Node>) node = &input;
+		  else return data_coinbase;
+
+		  if (not node->IsDict()) return data_coinbase;
+		  auto& trade = const_cast<Json::Dict&>(node->AsDict());
+		  data_coinbase.empty = false;
+		  if (auto found = trade.find("ask");
+				  found!=trade.end() && found->second.IsString()) {
+			  data_coinbase.ask = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = trade.find("bid");
+				  found!=trade.end() && found->second.IsString()) {
+			  data_coinbase.bid = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = trade.find("price");
+				  found!=trade.end() && found->second.IsString()) {
+			  data_coinbase.price = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = trade.find("size");
+				  found!=trade.end() && found->second.IsString()) {
+			  data_coinbase.size = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = trade.find("time");
+				  found!=trade.end() && found->second.IsString()) {
+			  data_coinbase.time = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  if (auto found = trade.find("trade_id");
+				  found!=trade.end() && found->second.IsInt()) {
+			  data_coinbase.trade_id = found->second.AsInt();
+		  }
+		  if (auto found = trade.find("volume");
+				  found!=trade.end() && found->second.IsString()) {
+			  data_coinbase.volume = std::move(const_cast<std::string&>(found->second.AsString()));
+		  }
+		  return data_coinbase;
+	  }
+
 
 	}//!namespace
   }//!namespace
